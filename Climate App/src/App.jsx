443bb968e2d6a0e8f82298e13ad0fec4f41b0db1
Daniel <skyrimdaniel11@gmail.com>
2023-03-06 01:32:31 -0300

    New component -> Form.jsx

--- Climate App/src/App.jsx
+++ Climate App/src/App.jsx
@@ -1,13 +1,15 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import './App.css'
+import Form from './components/Form'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <div className="App">
-   
+      <h1>Climate React App</h1>
+   <Form />
     </div>
   )
 }
---
+++ Climate App/src/components/Form.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+import './styles/Form.css'
+
+const Form = () => {
+  return (
+    <form>
+        <input type="text" placeholder='Digite a nome do local' />
+    </form>
+    
+  )
+}
+
+export default Form
